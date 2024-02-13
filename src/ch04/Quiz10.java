aaadaa781bb62b042c990f81ada510740454a25d
sseung519 <htc_desirehd@naver.com>
2024-02-13 17:28:05 +0900

    240213

--- src/ch04/Quiz10.java
+++ src/ch04/Quiz10.java
@@ -12,7 +12,7 @@ public class Quiz10 {
         while (bank) {
             System.out.println("----------------");
             System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
-            System.out.println("-----------------");
+            System.out.println("----------------");
 
             System.out.print("선택: ");
             String selectNum = scanner.nextLine();
