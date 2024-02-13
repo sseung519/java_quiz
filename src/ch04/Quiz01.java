1612320afa71d37dd6ca45af9ac3ac70660f19df
sseung519 <htc_desirehd@naver.com>
2024-02-13 17:35:13 +0900

    240213

--- src/ch04/Quiz01.java
+++ src/ch04/Quiz01.java
@@ -4,10 +4,10 @@ public class Quiz01 {
     public static void main(String[] args) {
         /*
         1. 10<x && x<20
-        2. ch == ' ' || ch == '\t'
+        2. ch != ' ' || ch != '\t'
         3. ch == 'x' || ch == 'X'
         4. '0' <= ch || ch <= '9'
-        5. 'A' <= ch || ch <= 'z'
+        5. 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z'
         6. year % 400 == 0 || year % 4 == 0 && year % 100 != 0
         7. powerOn != true
         8. str.equals("yes")
