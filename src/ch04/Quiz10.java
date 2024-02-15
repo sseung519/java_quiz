aeb0cfc580eccd5ec643f497de7e29260cb41a2e
sseung519 <htc_desirehd@naver.com>
2024-02-15 18:09:30 +0900

    240215

--- src/ch04/Quiz10.java
+++ src/ch04/Quiz10.java
@@ -33,6 +33,33 @@ public class Quiz10 {
         }
         System.out.println("프로그램 종료");
 
+        //풀이
+//        while (bank) {
+//            System.out.println("----------------");
+//            System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+//            System.out.println("----------------");
+//
+//            String selectNum = scanner.next();
+//
+//            switch (selectNum) {
+//                case "1":
+//                    System.out.print("예금할 금액: ");
+//                    balance += scanner.nextInt();
+//                    break;
+//                case "2":
+//                    System.out.print("출금할 금액: ");
+//                    balance -= scanner.nextInt();
+//                    break;
+//                case "3":
+//                    System.out.print("잔고: ");
+//                    System.out.print(balance);
+//                    break;
+//                case "4":
+//                    bank = false;
+//                    break;
+//            }
+//        }
+//        System.out.println("프로그램 종료");
     }
 }
 
---
+++ src/ch06/Quiz13_14/MemberExample.java
@@ -0,0 +1,8 @@
+package ch06.Quiz13_14;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member Glory = new Member("이영광", "glory");
+        System.out.println(Glory);
+    }
+}
