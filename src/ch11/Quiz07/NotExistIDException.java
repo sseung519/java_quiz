11a7eda27acb1c87ecf16e3b0797dfc549c3aa52
sseung519 <htc_desirehd@naver.com>
2024-03-04 14:43:01 +0900

    240304

---
+++ src/ch11/Quiz07/NotExistIDException.java
@@ -0,0 +1,10 @@
+package ch11.Quiz07;
+
+public class NotExistIDException extends Exception{
+    public NotExistIDException() {}
+
+    public NotExistIDException(String message) {
+        super(message);
+    }
+
+}
